de4f8e0af2c974b50bfde239fa0dab92ea90bc6f
namestesensei <christopherwright435@gmail.com>
2025-01-15 15:11:28 -0600

    adjust char issue with 3-flip confirmed under 79 char

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 """
-This module contains a function to transpose a 2D matrix.
+This module contains a function to transpose a 2D matr.
 """
 
 
 def matrix_transpose(matrix):
     """
     Returns the transpose of a 2D matrix.
-
     Args:
-        matrix (list of lists): The 2D matrix to transpose.
-
+        matrix (list of lists): The 2D matrix to tranpose.
     Returns:
-        list of lists: A new 2D matrix that is the transpose of the input matrix.
+        list of lists: A new 2D matrix is the tanspose of the input matrix.
     """
     return [
-        [row[i] for row in matrix]  # E201 fixed: No extra space after '['
+        [row[i] for row in matrix]
         for i in range(len(matrix[0]))
     ]
