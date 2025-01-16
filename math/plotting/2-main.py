5d1f0592eb6318d3f844f50081cb5e1ddb743d8b
namestesensei <christopherwright435@gmail.com>
2025-01-15 19:06:27 -0600

    Add 2-main.py to test Task 2

---
+++ math/plotting/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Main file to test the 2-change_scale module.
+"""
+
+change_scale = __import__('2-change_scale').change_scale
+
+if __name__ == "__main__":
+    change_scale()
