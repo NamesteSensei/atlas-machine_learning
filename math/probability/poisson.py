edae3d345d2b2c70794f43e94c13bfe8c4dcc3d5
namestesensei <christopherwright435@gmail.com>
2025-01-18 02:28:33 -0600

    added CDF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -13,7 +13,7 @@ class Poisson:
         """
         Initialize a Poisson distribution.
         data: list of data points (optional)
-        lambtha: expected number of occurrences in a given time frame
+        lambtha: expected number of occurrences in a given time frame (default 1.)
         """
         if data is None:
             if lambtha <= 0:
@@ -24,34 +24,44 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """
-        Calculate the PMF for a given number of successes (k).
+        Calculates the value of the PMF for a given number of successes (k).
         k: number of successes
         Returns: PMF value for k
         """
+        if k < 0:
+            return 0
         k = int(k)
+        return (self.lambtha ** k * (2.7182818285 ** -self.lambtha)) / self.factorial(k)
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of successes.
+        k: number of successes
+        Returns: CDF value for k
+        """
         if k < 0:
             return 0
 
-        return (self.lambtha ** k * self.exp_neg_lambda()) / self.factorial(k)
+        k = int(k)
+        cdf_sum = 0
 
-    def exp_neg_lambda(self):
-        """
-        Calculate e^(-lambda).
-        """
-        e = 2.7182818285
-        return e ** -self.lambtha
+        for i in range(k + 1):
+            cdf_sum += (self.lambtha ** i * (2.7182818285 ** -self.lambtha)) / \
+                       self.factorial(i)
 
-    def factorial(self, num):
+        return cdf_sum
+
+    def factorial(self, n):
         """
-        Calculate the factorial of a number.
+        Helper method to calculate factorial of n.
         """
-        if num == 0:
+        if n == 0:
             return 1
         result = 1
-        for i in range(1, num + 1):
+        for i in range(1, n + 1):
             result *= i
         return result
