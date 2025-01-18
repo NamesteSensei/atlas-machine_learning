84de601aaedbfde54c10aa66c67fcbe3caa0bc67
namestesensei <christopherwright435@gmail.com>
2025-01-18 03:25:09 -0600

    correct char

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Poisson Distribution module.
+This module defines the Poisson class to represent a Poisson distribution.
 """
 
 
@@ -11,9 +11,17 @@ class Poisson:
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Initialize a Poisson distribution.
-        data: list of data points (optional)
-        lambtha: expected number of occurrences in a given time frame
+        Initialize the Poisson distribution.
+
+        Args:
+            data (list): A list of data to estimate the distribution.
+            lambtha (float): The expected number of occurrences in a
+                             given time frame.
+
+        Raises:
+            TypeError: If data is not a list.
+            ValueError: If data contains fewer than two values.
+            ValueError: If lambtha is not positive.
         """
         if data is None:
             if lambtha <= 0:
@@ -24,11 +32,17 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """
-        Calculate the PMF for a given number of successes (k).
+        Calculate the PMF value for a given number of successes.
+
+        Args:
+            k (int): The number of successes.
+
+        Returns:
+            float: The PMF value for k.
         """
         if k < 0:
             return 0
@@ -36,38 +50,28 @@ class Poisson:
         factorial = 1
         for i in range(1, k + 1):
             factorial *= i
-        pmf = (self.lambtha ** k) * (self.exp_neg_lambtha()) / factorial
-        return pmf
+        pmf_value = ((self.lambtha ** k) *
+                     (2.7182818285 ** -self.lambtha) / factorial)
+        return pmf_value
 
     def cdf(self, k):
         """
-        Calculate the CDF for a given number of successes (k).
+        Calculate the CDF value for a given number of successes.
+
+        Args:
+            k (int): The number of successes.
+
+        Returns:
+            float: The CDF value for k.
         """
         if k < 0:
             return 0
         k = int(k)
         cumulative = 0
-        for i in range(k + 1):
-            # Manual factorial calculation
+        for i in range(0, k + 1):
             factorial = 1
             for j in range(1, i + 1):
                 factorial *= j
-
-            cumulative += (
-                (self.lambtha ** i * self.exp_neg_lambtha()) / factorial
-            )
+            cumulative += ((self.lambtha ** i) *
+                           (2.7182818285 ** -self.lambtha) / factorial)
         return cumulative
-
-    def exp_neg_lambtha(self):
-        """
-        Helper function to calculate e^(-lambtha) using approximation.
-        """
-        terms = 20  # Number of terms for the approximation
-        result = 1
-        power = 1
-        factorial = 1
-        for n in range(1, terms + 1):
-            power *= -self.lambtha
-            factorial *= n
-            result += power / factorial
-        return result
