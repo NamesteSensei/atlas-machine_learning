597497673d368a75f92717a92c594294b712d4f6
namestesensei <christopherwright435@gmail.com>
2025-01-19 23:19:05 -0600

    10

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,48 +1,53 @@
 #!/usr/bin/env python3
 """
-Binomial Distribution module.
+This module defines the Binomial class to represent a binomial distribution.
+
+The binomial distribution models the number of successes in n independent
+Bernoulli trials with probability p of success.
 """
 
 
 class Binomial:
+    """
+    Represents a binomial distribution.
+
+    Attributes:
+        n (int): The number of Bernoulli trials.
+        p (float): The probability of a success.
+    """
+
+    def __init__(self, data=None, n=1, p=0.5):
         """
-            Represents a Binomial distribution.
-                """
+        Initialize the Binomial distribution.
 
-                    def __init__(self, data=None, n=1, p=0.5):
-                                """
-                                        Initialize a Binomial distribution.
-                                                """
-                                                        if data is None:
-                                                                        if n <= 0:
-                                                                                            raise ValueError("n must be a positive value")
-                                                                                                    if not (0 < p < 1):
-                                                                                                                        raise ValueError("p must be greater than 0 and less than 1")
-                                                                                                                                self.n = int(n)
-                                                                                                                                            self.p = float(p)
-                                                                                                                                                    else:
-                                                                                                                                                                    if not isinstance(data, list):
-                                                                                                                                                                                        raise TypeError("data must be a list")
-                                                                                                                                                                                                if len(data) < 2:
-                                                                                                                                                                                                                    raise ValueError("data must contain multiple values")
-                                                                                                                                                                                                                            mean = sum(data) / len(data)
-                                                                                                                                                                                                                                        variance = sum((x - mean)**2 for x in data) / len(data)
-                                                                                                                                                                                                                                                    self.p = 1 - (variance / mean)
-                                                                                                                                                                                                                                                                self.n = round(mean / self.p)
+        If `data` is provided, the number of trials (n) and the probability
+        of success (p) are calculated from the data.
 
-                                                                                                                                                                                                                                                                    def pmf(self, k):
-                                                                                                                                                                                                                                                                                """
-                                                                                                                                                                                                                                                                                        Calculate the PMF for a given number of successes (k).
-                                                                                                                                                                                                                                                                                                """
-                                                                                                                                                                                                                                                                                                        from math import comb
-                                                                                                                                                                                                                                                                                                                k = int(k)
-                                                                                                                                                                                                                                                                                                                        if k < 0 or k > self.n:
-                                                                                                                                                                                                                                                                                                                                        return 0
-                                                                                                                                                                                                                                                                                                                                            return comb(self.n, k) * (self.p**k) * ((1 - self.p)**(self.n - k))
+        Args:
+            data (list): A list of data to estimate the distribution.
+            n (int): The number of Bernoulli trials.
+            p (float): The probability of success.
 
-                                                                                                                                                                                                                                                                                                                                            def cdf(self, k):
-                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                Calculate the CDF for a given number of successes (k).
-                                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                                return sum(self.pmf(i) for i in range(int(k) + 1))
-                                                                                                                                                                                                                                                                                                                    
+        Raises:
+            TypeError: If `data` is not a list.
+            ValueError: If `data` contains fewer than two values.
+            ValueError: If `n` is not a positive integer.
+            ValueError: If `p` is not in the range (0, 1).
+        """
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            self.p = 1 - (variance / mean)
+            self.n = round(mean / self.p)
+            self.p = mean / self.n
