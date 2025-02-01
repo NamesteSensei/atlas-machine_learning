270f43e58975f3731b46e9f87c7f05b86dc8eaf1
namestesensei <christopherwright435@gmail.com>
2025-01-31 22:17:56 -0600

    corrected pycodestyle error

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-""" Neuron Class for Binary Classification """
-
+"""
+Neuron Class for Binary Classification
+"""
 import numpy as np
+
 
 class Neuron:
     """Defines a single neuron performing binary classification"""
 
     def __init__(self, nx):
-        """
-        Initialize a Neuron.
+        """Initialize a Neuron.
 
         Args:
             nx (int): Number of input features.
@@ -25,3 +26,16 @@ class Neuron:
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
+
+
+# Test script
+if __name__ == "__main__":
+    np.random.seed(0)
+    nx = 784
+    neuron = Neuron(nx)
+    print(neuron.W)
+    print(neuron.W.shape)
+    print(neuron.b)
+    print(neuron.A)
+    neuron.A = 10
+    print(neuron.A)
