757413ededf24e1973e71e653c4453ac9f9e1a3f
namestesensei <christopherwright435@gmail.com>
2025-01-31 23:48:22 -0600

    pycodestyle validation correction

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -3,17 +3,12 @@
 Neuron Class for Binary Classification with Forward Propagation
 """
 import numpy as np
-
-
 class Neuron:
     """Defines a single neuron performing binary classification"""
-
     def __init__(self, nx):
         """Initialize a Neuron.
-
         Args:
             nx (int): Number of input features.
-
         Raises:
             TypeError: If nx is not an integer.
             ValueError: If nx is less than 1.
@@ -22,32 +17,25 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-
     @property
     def W(self):
         """Getter for W"""
         return self.__W
-
     @property
     def b(self):
         """Getter for b"""
         return self.__b
-
     @property
     def A(self):
         """Getter for A"""
         return self.__A
-
     def forward_prop(self, X):
         """Calculates the forward propagation of the neuron.
-        
         Args:
             X (numpy.ndarray): Input data of shape (nx, m).
-        
         Returns:
             numpy.ndarray: Activated output (A) of shape (1, m).
         """
