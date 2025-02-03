9898419fffed7c3b0a5d96f6e28973c102f2b448
namestesensei <christopherwright435@gmail.com>
2025-02-03 16:08:28 -0600

    Fixed Pycodestyle errors for Task 5

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 class Neuron:
-    
+    """Defines a single neuron performing binary classification"""
+
     def __init__(self, nx):
         """Initialize Neuron"""
         if not isinstance(nx, int):
@@ -12,17 +14,20 @@ class Neuron:
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
-    
+
     @property
     def W(self):
+        """Getter for weights"""
         return self.__W
 
     @property
     def b(self):
+        """Getter for bias"""
         return self.__b
 
     @property
     def A(self):
+        """Getter for activated output"""
         return self.__A
 
     def forward_prop(self, X):
