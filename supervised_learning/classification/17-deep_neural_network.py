8ef2d6fcfc0afab1e06aad4e7528a0a52309989b
namestesensei <christopherwright435@gmail.com>
2025-02-07 01:29:44 -0600

    17.5

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -3,21 +3,21 @@ import numpy as np
 
 class DeepNeuralNetwork:
     def __init__(self, nx, layers):
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        For x in layers:
-            if not isinstance(x, int) or x < 1:
+        for nodes in layers:
+            if type(nodes) is not int or nodes < 1:
                 raise TypeError("layers must be a list of positive integers")
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
         For i in range(self.__L):
             if i == 0:
-                self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.__weights["W1"] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
             else:
                 self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
             self.__weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
@@ -25,11 +25,12 @@ class DeepNeuralNetwork:
     @property
     def L(self):
         return self.__L
-    
+
     @property
     def cache(self):
         return self.__cache
-    
+
     @property
     def weights(self):
         return self.__weights
+
