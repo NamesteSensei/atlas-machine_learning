2f68e2f113f292cbc21f6c6dc72131f35e381c43
namestesensei <christopherwright435@gmail.com>
2025-02-07 01:24:01 -0600

    17.5

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,44 +1,35 @@
 #!/usr/bin/env python3
 import numpy as np
+
 class DeepNeuralNetwork:
-    
     def __init__(self, nx, layers):
-        
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if (not isinstance(layers, list) or len(layers) == 0:
-            raise TypedError("layers must be a list of positive integers")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        For x in layers:
+            if not isinstance(x, int) or x < 1:
+                raise TypeError("layers must be a list of positive integers")
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        prev_layer_size = nx  # Tracks size of the previous layer
-        for layer in range(1, self.__L + 1):
-            self.__weights[f"W{layer}"] = (
-                np.random.randn(layers[layer - 1], prev_layer_size) *
-                np.sqrt(2 / prev_layer_size)
-            )
-            self.__weights[f"b{layer}"] = np.zeros((layers[layer - 1], 1))
-            prev_layer_size = layers[layer - 1]  # Update next layer
+        For i in range(self.__L):
+            if i == 0:
+                self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+            self.__weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+    
     @property
     def L(self):
-        
         return self.__L
+    
     @property
     def cache(self):
-        
         return self.__cache
+    
     @property
     def weights(self):
-        
         return self.__weights
-    def forward_prop(self, X):
-        
-        self.__cache["A0"] = X  # Store input data
-        prev_activation = X
-        for layer in range(1, self.__L + 1):
-            Z = np.matmul(self.__weights[f"W{layer}"], prev_activation) + self.__weights[f"b{layer}"]
-            prev_activation = 1 / (1 + np.exp(-Z))  # Sigmoid activation
-            self.__cache[f"A{layer}"] = prev_activation
-        return prev_activation, self.__cache
