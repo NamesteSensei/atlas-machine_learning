81f73f49c8a75f9279c3043f7572b2d63e81adea
namestesensei <christopherwright435@gmail.com>
2025-02-09 22:00:28 -0600

    17.0

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -1,36 +1,66 @@
-#!/usr/bin/env python3
 import numpy as np
 
+
 class DeepNeuralNetwork:
+    """Defines a deep neural network for binary classification"""
+
     def __init__(self, nx, layers):
+        """Class constructor"""
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        For nodes in layers:
-            if type(nodes) is not int or nodes < 1:
-                raise TypeError("layers must be a list of positive integers")
+        if any(type(i) is not int or i <= 0 for i in layers):
+            raise TypeError("layers must be a list of positive integers")
+
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        for i in range(self.__L):
-            if i == 0:
-                self.__weights["W1"] = np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
-            else:
-                self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            self.__weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
-    
+
+        prev_units = nx
+        step = 1
+        while step <= self.__L:
+            self.__weights[f'W{step}'] = (
+                np.random.randn(layers[step - 1], prev_units) * np.sqrt(2 / prev_units)
+            )
+            self.__weights[f'b{step}'] = np.zeros((layers[step - 1], 1))
+            prev_units = layers[step - 1]
+            step += 1
+
     @property
     def L(self):
+        """Getter for L"""
         return self.__L
 
     @property
     def cache(self):
+        """Getter for cache"""
         return self.__cache
 
     @property
     def weights(self):
+        """Getter for weights"""
         return self.__weights
 
+    def forward_prop(self, X):
+        """
+        Perform forward propagation using sigmoid activation function.
+        X: numpy.ndarray - input data of shape (nx, m)
+        Returns: the activated output and the cache
+        """
+        self.__cache["A0"] = X  # Store input data
+        step = 1
+        while step <= self.__L:
+            W = self.__weights[f'W{step}']
+            b = self.__weights[f'b{step}']
+            A_prev = self.__cache[f'A{step - 1}']
+
+            Z = np.dot(W, A_prev) + b  # Linear step
+            A = 1 / (1 + np.exp(-Z))  # Sigmoid activation
+            self.__cache[f'A{step}'] = A  # Store activation in cache
+
+            step += 1
+
+        return A, self.__cache
