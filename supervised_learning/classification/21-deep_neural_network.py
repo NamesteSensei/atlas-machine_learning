c8ad7116ea373fe8ca946c09b7095007033264c9
namestesensei <christopherwright435@gmail.com>
2025-02-12 22:31:30 -0600

    21.2

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 import numpy as np
 
-"""
-Deep Neural Network module for binary classification.
-"""
-
 class DeepNeuralNetwork:
     """ Defines a deep neural network performing binary classification. """
 
     def __init__(self, nx, layers):
         """ Initialize the network """
         np.random.seed(0)  # Set fixed seed for consistency
-        
+
         if type(nx) is not int or nx < 1:
             raise ValueError("nx must be a positive integer")
-        
-        if type(layers) is not list or len(layers) == 0 or not all(isinstance(i, int) and i > 0 for i in layers):
+
+        if (type(layers) is not list or len(layers) == 0 or 
+            not all(isinstance(i, int) and i > 0 for i in layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)  # Number of layers
@@ -23,7 +20,7 @@ class DeepNeuralNetwork:
         self.__weights = {}
 
         prev = nx
-        for i in range(self.__L):
+        for i in range(self.__L):  # 🔴 Loop 1: Weight Initialization
             self.__weights["W{}".format(i + 1)] = np.random.randn(layers[i], prev) * np.sqrt(2 / prev)
             self.__weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
             prev = layers[i]
@@ -43,7 +40,7 @@ class DeepNeuralNetwork:
     def forward_prop(self, X):
         """ Computes forward propagation """
         self.__cache["A0"] = X
-        for i in range(self.__L):
+        for i in range(self.__L):  # 🔴 Loop 2: Forward Propagation
             W = self.__weights["W{}".format(i + 1)]
             b = self.__weights["b{}".format(i + 1)]
             A_prev = self.__cache["A{}".format(i)]
@@ -68,26 +65,6 @@ class DeepNeuralNetwork:
         """ Performs gradient descent weight updates """
         m = Y.shape[1]
         dZ = cache["A{}".format(self.__L)] - Y
-
-        for i in range(self.__L, 0, -1):
+        for i in range(self.__L, 0, -1):  # 🔴 Loop 3: Gradient Descent Updates
             A_prev = cache["A{}".format(i - 1)]
             dW = np.matmul(dZ, A_prev.T) / m
-            db = np.sum(dZ, axis=1, keepdims=True) / m
-            self.__weights["W{}".format(i)] -= alpha * dW
-            self.__weights["b{}".format(i)] -= alpha * db
-            if i > 1:
-                A = cache["A{}".format(i - 1)]
-                dZ = np.matmul(self.__weights["W{}".format(i)].T, dZ) * (A * (1 - A))
-
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """ Trains the neural network """
-        if type(iterations) is not int or iterations <= 0:
-            raise ValueError("iterations must be a positive integer")
-        if type(alpha) is not float or alpha <= 0:
-            raise ValueError("alpha must be positive")
-
-        for _ in range(iterations):
-            A, cache = self.forward_prop(X)
-            self.gradient_descent(Y, cache, alpha)
-        
-        return self.evaluate(X, Y)
