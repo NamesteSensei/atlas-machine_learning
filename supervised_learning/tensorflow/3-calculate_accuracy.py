Fixed accuracy calculation in Task 3

supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -9,17 +9,12 @@ tf.disable_eager_execution()
 def calculate_accuracy(y, y_pred):
     """
     Calculates the accuracy of a prediction.
-
     Args:
         y: Placeholder for the true labels
         y_pred: Tensor containing the model's predictions
-
     Returns:
         Tensor containing the accuracy
     """
-    correct_predictions = tf.equal(
-        tf.argmax(y_pred, axis=1),
-        tf.argmax(y, axis=1)
-    )
+    correct_predictions = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
     return accuracy