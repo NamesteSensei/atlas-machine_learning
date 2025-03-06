22335fa6b328c6245f4197c612aafedab1848ff1
namestesensei <christopherwright435@gmail.com>
2025-03-06 01:50:57 -0600

    fixed pycodestyle error4.3

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+
 """
 Module: 4-train
 Trains a model using mini-batch gradient descent.
 """
 
 import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
