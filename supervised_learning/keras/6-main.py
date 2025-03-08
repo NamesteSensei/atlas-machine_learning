59cd049d65ddf19bf3ccc16de5867aca5c1c5728
namestesensei <christopherwright435@gmail.com>
2025-03-07 20:40:45 -0600

    pycodestyle error2.0

--- supervised_learning/keras/6-main.py
+++ supervised_learning/keras/6-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Main file for testing 6-train module
+Main file for Task 6: Early Stopping in model training.
 """
 
-# Imports
+# Standard library imports
 import os
 import random
 import numpy as np
@@ -13,11 +13,12 @@ import tensorflow as tf
 SEED = 8
 os.environ['PYTHONHASHSEED'] = str(SEED)
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 random.seed(SEED)
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
 
-# Module Imports
+# Imports from local modules
 build_model = __import__('1-input').build_model
 optimize_model = __import__('2-optimize').optimize_model
 one_hot = __import__('3-one_hot').one_hot
@@ -25,20 +26,32 @@ train_model = __import__('6-train').train_model
 
 if __name__ == '__main__':
     datasets = np.load('MNIST.npz')
-    X_train = datasets['X_train'].reshape(-1, 784)
-    Y_train = one_hot(datasets['Y_train'])
-    X_valid = datasets['X_valid'].reshape(-1, 784)
-    Y_valid = one_hot(datasets['Y_valid'])
+    X_train = datasets['X_train'].reshape(datasets['X_train'].shape[0], -1)
+    Y_train = datasets['Y_train']
+    Y_train_oh = one_hot(Y_train)
 
+    X_valid = datasets['X_valid'].reshape(datasets['X_valid'].shape[0], -1)
+    Y_valid = datasets['Y_valid']
+    Y_valid_oh = one_hot(Y_valid)
+
+    lambtha = 0.0001
+    keep_prob = 0.95
     network = build_model(
-        784, [256, 256, 10], ['relu', 'relu', 'softmax'],
-        lambtha=0.0001, keep_prob=0.95
+        784, [256, 256, 10],
+        ['relu', 'relu', 'softmax'],
+        lambtha, keep_prob
     )
-    optimize_model(network, alpha=0.001, beta1=0.9, beta2=0.999)
+
+    alpha = 0.001
+    beta1 = 0.9
+    beta2 = 0.999
+    optimize_model(network, alpha, beta1, beta2)
+
+    batch_size = 64
+    epochs = 30
 
     train_model(
-        network, X_train, Y_train,
-        batch_size=64, epochs=30,
-        validation_data=(X_valid, Y_valid),
+        network, X_train, Y_train_oh, batch_size, epochs,
+        validation_data=(X_valid, Y_valid_oh),
         early_stopping=True, patience=3
     )
