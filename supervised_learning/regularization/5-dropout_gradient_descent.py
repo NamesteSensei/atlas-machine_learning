e99797a7b1192dd387b08de44fe3aacb387a6c3c
namestesensei <christopherwright435@gmail.com>
2025-09-09 15:11:32 -0500

    5.1

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Gradient descent with Dropout regularization.
+Gradient Descent with Dropout regularization.
 """
 
 import numpy as np
@@ -8,34 +8,39 @@ import numpy as np
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Updates weights of neural net using dropout + gradient descent.
+    Updates the weights of a neural network using
+    gradient descent with Dropout regularization.
 
     Args:
-        Y: one-hot labels, shape (classes, m)
-        weights: dict of weights and biases
-        cache: dict of activations and dropout masks
+        Y: one-hot matrix of correct labels (classes, m)
+        weights: dictionary of weights and biases
+        cache: dictionary of all intermediary values of the network
         alpha: learning rate
-        keep_prob: probability to keep a node
+        keep_prob: probability that a node will be kept
         L: number of layers
 
     Returns:
         None. Updates weights in-place.
     """
     m = Y.shape[1]
-    dz = cache['A' + str(L)] - Y
+    dZ = cache["A" + str(L)] - Y  # softmax derivative
 
-    for i in reversed(range(1, L + 1)):
-        A_prev = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
+    for l in reversed(range(1, L + 1)):
+        A_prev = cache["A" + str(l - 1)]
+        W = weights["W" + str(l)]
 
-        dw = (1 / m) * np.matmul(dz, A_prev.T)
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        weights['W' + str(i)] -= alpha * dw
-        weights['b' + str(i)] -= alpha * db
+        weights["W" + str(l)] -= alpha * dW
+        weights["b" + str(l)] -= alpha * db
 
-        if i > 1:
-            da = np.matmul(W.T, dz)
-            dz = da * (1 - cache['A' + str(i - 1)] ** 2)
-            dz *= cache['D' + str(i - 1)]
-            dz /= keep_prob
+        if l > 1:
+            # Backprop through tanh + dropout
+            dA_prev = np.matmul(W.T, dZ)
+            D = cache["D" + str(l - 1)]
+            dA_prev *= D
+            dA_prev /= keep_prob
+
+            A_prev = cache["A" + str(l - 1)]
+            dZ = dA_prev * (1 - A_prev ** 2)  # tanh derivative
