a4f6dba456994cffb62eea5ef5721e6d72562926
namestesensei <christopherwright435@gmail.com>
2025-09-16 18:58:16 -0500

    2.1

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -4,7 +4,7 @@ Module that calculates the marginal probability of
 observing data given priors and hypotheses.
 """
 import numpy as np
-intersection = __import__('1-intersection').intersection
+likelihood = __import__('0-likelihood').likelihood
 
 
 def marginal(x, n, P, Pr):
@@ -23,5 +23,26 @@ def marginal(x, n, P, Pr):
     Raises:
         ValueError, TypeError: according to spec
     """
-    inter = intersection(x, n, P, Pr)
-    return np.sum(inter)
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the "
+                        "same shape as P")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the "
+                         "range [0, 1]")
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the "
+                         "range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    like = likelihood(x, n, P)
+    return np.sum(like * Pr)
