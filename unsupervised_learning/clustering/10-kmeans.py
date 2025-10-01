70ca1cf4a6ffca3a42c2b59df8ce7983bdb22ced
namestesensei <christopherwright435@gmail.com>
2025-10-01 15:49:43 -0500

    10.4

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
 """
-This module implements K-means clustering using scikit-learn's KMeans.
-
-Only the sklearn.cluster module is imported as per project constraints.
-
-Function:
-    kmeans(X, k): Fits K-means to the data and returns cluster centroids
-    and index labels for each data point.
+K-means clustering using sklearn's KMeans estimator.
 """
-
 import sklearn.cluster
 
 
 def kmeans(X, k):
     """
-    Performs K-means on a dataset.
+    Runs K-means on input data X with k clusters.
 
     Args:
-        X (numpy.ndarray): shape (n, d) dataset.
-        k (int): number of clusters.
+        X: ndarray of shape (n, d), input samples.
+        k: int, number of clusters.
 
     Returns:
-        C (numpy.ndarray): shape (k, d), centroid coordinates.
-        clss (numpy.ndarray): shape (n,), index of the cluster each point belongs to.
+        C: ndarray of shape (k, d), cluster centers.
+        clss: ndarray of shape (n,), cluster index per sample.
     """
     model = sklearn.cluster.KMeans(n_clusters=k)
     model.fit(X)
-    C = model.cluster_centers_
-    clss = model.labels_
-
-    return C, clss
+    return model.cluster_centers_, model.labels_
