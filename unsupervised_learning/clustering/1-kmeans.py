b1952057a00bfcf9d20f2e7058801f54b3547ba5
namestesensei <christopherwright435@gmail.com>
2025-10-01 16:41:32 -0500

    redo1.1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,50 +1,39 @@
 #!/usr/bin/env python3
-"""
-K-means clustering algorithm from scratch.
-"""
+"""Performs K-means clustering on a dataset."""
 import numpy as np
 
 
 def kmeans(X, k, iterations=1000):
-    """
-    Performs K-means on dataset X.
-
-    Args:
-        X: ndarray of shape (n, d)
-        k: number of clusters
-        iterations: max number of updates
-
-    Returns:
-        C: ndarray (k, d) - centroid means
-        clss: ndarray (n,) - cluster assignments
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(k, int) or k <= 0:
-        return None, None
-    if not isinstance(iterations, int) or iterations <= 0:
+    """Executes K-means clustering on input data."""
+    if (not isinstance(X, np.ndarray) or len(X.shape) != 2 or
+        not isinstance(k, int) or k <= 0 or
+        not isinstance(iterations, int) or iterations <= 0):
         return None, None
 
     n, d = X.shape
-
-    min_vals = np.min(X, axis=0)
-    max_vals = np.max(X, axis=0)
-    C = np.random.uniform(min_vals, max_vals, (k, d))
+    min_, max_ = np.min(X, axis=0), np.max(X, axis=0)
+    C = np.random.uniform(min_, max_, size=(k, d))
 
     for _ in range(iterations):
         D = np.linalg.norm(X[:, None] - C[None, :], axis=2)
         clss = np.argmin(D, axis=1)
 
         new_C = np.copy(C)
-        for i in range(k):
-            points = X[clss == i]
-            if points.shape[0] == 0:
-                new_C[i] = np.random.uniform(min_vals, max_vals)
-            else:
-                new_C[i] = np.mean(points, axis=0)
+        i = 0
+        while i < k:
+            pts = X[clss == i]
+            new_C[i] = (np.mean(pts, axis=0) if pts.shape[0] > 0
+                        else np.random.uniform(min_, max_))
+            i += 1
 
         if np.allclose(C, new_C):
             break
         C = new_C
+
+    idx = np.lexsort(C.T)
+    C = C[idx]
+    label_map = np.zeros(k, dtype=int)
+    label_map[idx] = np.arange(k)
+    clss = label_map[clss]
 
     return C, clss
