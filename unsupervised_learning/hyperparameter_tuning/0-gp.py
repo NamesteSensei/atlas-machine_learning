98d5b7d9ac38b847b4e8689894308ee459a7daf1
namestesensei <christopherwright435@gmail.com>
2025-10-12 21:15:22 -0500

    0.1

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -7,17 +7,17 @@ import numpy as np
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process"""
 
-    def __init__(self, X_init, Y_init, length_scale=1, sigma_f=1):
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):  # noqa: E741
         """Initialize Gaussian Process
 
         X_init: np.ndarray of shape (t, 1) - initial inputs
         Y_init: np.ndarray of shape (t, 1) - initial outputs
-        length_scale: float - kernel length parameter
+        l: float - kernel length parameter
         sigma_f: float - output standard deviation
         """
         self.X = X_init
         self.Y = Y_init
-        self.length_scale = length_scale
+        self.l = l  # noqa: E741
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
 
@@ -25,5 +25,4 @@ class GaussianProcess:
         """Calculate covariance matrix using RBF kernel"""
         sqdist = (np.sum(X1 ** 2, 1).reshape(-1, 1)
                   + np.sum(X2 ** 2, 1) - 2 * np.dot(X1, X2.T))
-        return (self.sigma_f ** 2
-                * np.exp(-0.5 / self.length_scale ** 2 * sqdist))
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
