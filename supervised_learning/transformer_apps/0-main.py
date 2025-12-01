f3f93b7c2dc583ce759d5ba19ad4ac35b0db36d5
namestesensei <christopherwright435@gmail.com>
2025-12-01 12:52:47 -0600

    changed the output from 5 lines to 2 lines

--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -1,33 +1,13 @@
 #!/usr/bin/env python3
-"""
-Main script to test the Dataset class.
-"""
 
-import importlib.util
-import sys
+Dataset = __import__('0-dataset').Dataset
 
-# Dynamically load 0-dataset.py
-spec = importlib.util.spec_from_file_location("dataset", "0-dataset.py")
-dataset = importlib.util.module_from_spec(spec)
-sys.modules["dataset"] = dataset
-spec.loader.exec_module(dataset)
-
-Dataset = dataset.Dataset
-
-
-if __name__ == "__main__":
-    data = Dataset()
-
-    print("\nSample training data:")
-    for pt, en in data.data_train.take(1):
-        print("PT:", pt.numpy().decode("utf-8"))
-        print("EN:", en.numpy().decode("utf-8"))
-
-    print("\nSample validation data:")
-    for pt, en in data.data_valid.take(1):
-        print("PT:", pt.numpy().decode("utf-8"))
-        print("EN:", en.numpy().decode("utf-8"))
-
-    print("\nTokenizer objects:")
-    print(type(data.tokenizer_pt))
-    print(type(data.tokenizer_en))
+data = Dataset()
+for pt, en in data.data_train.take(1):
+    print(pt.numpy().decode('utf-8'))
+    print(en.numpy().decode('utf-8'))
+for pt, en in data.data_valid.take(1):
+    print(pt.numpy().decode('utf-8'))
+    print(en.numpy().decode('utf-8'))
+print(type(data.tokenizer_pt))
+print(type(data.tokenizer_en))
