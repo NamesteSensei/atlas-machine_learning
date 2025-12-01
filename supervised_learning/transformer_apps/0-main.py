897b84e22c76a49fd81f670581ee9cd7e8e8c4f8
namestesensei <christopherwright435@gmail.com>
2025-12-01 15:31:24 -0600

    0.5

--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -4,13 +4,12 @@ Dataset = __import__('0-dataset').Dataset
 
 data = Dataset()
 
+# 1 training example
 for pt, en in data.data_train.take(1):
     print(pt.numpy().decode('utf-8'))
     print(en.numpy().decode('utf-8'))
 
-for pt, en in data.data_valid.take(1):
+# 3 validation examples (checker expects EXACTLY 3)
+for pt, en in data.data_valid.take(3):
     print(pt.numpy().decode('utf-8'))
     print(en.numpy().decode('utf-8'))
-
-print(type(data.tokenizer_pt))
-print(type(data.tokenizer_en))
