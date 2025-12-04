5a7da85ca9d843d321b1bd8ad947adc693eb81d9
namestesensei <christopherwright435@gmail.com>
2025-12-03 23:36:07 -0600

    1.0

--- supervised_learning/transformer_apps/1-dataset.py
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -1,55 +1,56 @@
 #!/usr/bin/env python3
-"""Dataset class for translating Portuguese to English"""
+"""Dataset with encode"""
 
-import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
+import transformers
 import numpy as np
-from tensorflow_datasets.deprecated.text import SubwordTextEncoder
 
 
 class Dataset:
-    """Loads and prepares a dataset for machine translation"""
+    """Loads data and tokenizers"""
 
     def __init__(self):
-        """Class constructor"""
-        examples, _ = tfds.load(
+        """Loads train and validation data"""
+        self.data_train = tfds.load(
             'ted_hrlr_translate/pt_to_en',
-            with_info=True,
-            as_supervised=True
-        )
+            split='train', as_supervised=True)
 
-        self.data_train = examples['train']
-        self.data_valid = examples['validation']
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='validation', as_supervised=True)
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
-        """Creates subword tokenizers for Portuguese and English"""
+        """Creates pretrained tokenizers"""
+        tok_pt = transformers.AutoTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased",
+            use_fast=True)
 
-        pt_sentences = []
-        en_sentences = []
+        tok_en = transformers.AutoTokenizer.from_pretrained(
+            "bert-base-uncased",
+            use_fast=True)
 
-        for pt, en in tfds.as_numpy(data):
-            pt_sentences.append(pt.decode('utf-8'))
-            en_sentences.append(en.decode('utf-8'))
-
-        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
-            pt_sentences, target_vocab_size=2**13)
-        tokenizer_en = SubwordTextEncoder.build_from_corpus(
-            en_sentences, target_vocab_size=2**13)
-
-        return tokenizer_pt, tokenizer_en
+        return tok_pt, tok_en
 
     def encode(self, pt, en):
-        """Encodes Portuguese and English sentences into token IDs"""
+        """Encodes sentences with start and end tokens"""
+        pt_tokens = self.tokenizer_pt.encode(
+            pt.numpy().decode("utf-8"),
+            add_special_tokens=False)
 
-        vocab_size_pt = self.tokenizer_pt.vocab_size
-        vocab_size_en = self.tokenizer_en.vocab_size
+        en_tokens = self.tokenizer_en.encode(
+            en.numpy().decode("utf-8"),
+            add_special_tokens=False)
 
-        pt_tokens = [vocab_size_pt] + self.tokenizer_pt.encode(
-            pt.numpy().decode('utf-8')) + [vocab_size_pt + 1]
-        en_tokens = [vocab_size_en] + self.tokenizer_en.encode(
-            en.numpy().decode('utf-8')) + [vocab_size_en + 1]
+        pt_start = self.tokenizer_pt.vocab_size
+        pt_end = pt_start + 1
 
-        return np.array(pt_tokens), np.array(en_tokens)
+        en_start = self.tokenizer_en.vocab_size
+        en_end = en_start + 1
+
+        pt_out = [pt_start] + pt_tokens + [pt_end]
+        en_out = [en_start] + en_tokens + [en_end]
+
+        return np.array(pt_out), np.array(en_out)
--- supervised_learning/transformer_apps/1-main.py
+++ supervised_learning/transformer_apps/1-main.py
@@ -3,7 +3,9 @@
 Dataset = __import__('1-dataset').Dataset
 
 data = Dataset()
+
 for pt, en in data.data_train.take(1):
     print(data.encode(pt, en))
+
 for pt, en in data.data_valid.take(1):
     print(data.encode(pt, en))
