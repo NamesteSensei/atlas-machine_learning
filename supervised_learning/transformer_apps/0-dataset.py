da3450789bff3b5a874d203d4bab1a31583a1db9
namestesensei <christopherwright435@gmail.com>
2025-12-03 23:15:35 -0600

    newday 0.0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,37 +1,28 @@
 #!/usr/bin/env python3
-"""Dataset class for machine translation using TED Talks pt_to_en"""
+"""Dataset class for Portuguese to English translation"""
+
 import tensorflow_datasets as tfds
-from transformers import AutoTokenizer
+import transformers
 
 
 class Dataset:
-    """
-    Loads and prepares dataset for machine translation using pre-trained tokenizers.
-    """
+    """Loads and prepares the translation dataset"""
 
     def __init__(self):
-        """
-        Initializes Dataset instance:
-        - Loads train and validation splits
-        - Creates subword tokenizers for both Portuguese and English
-        """
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation', as_supervised=True)
+        """Loads training and validation data, sets tokenizers"""
+        self.data_train = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='train', as_supervised=True)
+        self.data_valid = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            split='validation', as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
-        """
-        Creates subword tokenizers for the dataset using pre-trained models.
-        Args:
-            data: tf.data.Dataset, tuples of (pt, en)
-        Returns:
-            Tuple of (Portuguese tokenizer, English tokenizer)
-        """
-        tokenizer_pt = AutoTokenizer.from_pretrained(
+        """Creates tokenizers from pre-trained models"""
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
             "neuralmind/bert-base-portuguese-cased", use_fast=True)
-        tokenizer_en = AutoTokenizer.from_pretrained(
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
             "bert-base-uncased", use_fast=True)
         return tokenizer_pt, tokenizer_en
--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-"""Main file to test Dataset class for TED translation dataset"""
+"""Test Dataset class"""
 
 Dataset = __import__('0-dataset').Dataset
 
-def main():
-    """
-    Runs sample tests on the Dataset class to confirm loading and tokenizers.
-    """
-    data = Dataset()
+data = Dataset()
 
-    # Print one sample from training set
-    for pt, en in data.data_train.take(1):
-        print(pt.numpy().decode('utf-8'))
-        print(en.numpy().decode('utf-8'))
+for pt, en in data.data_train.take(1):
+    print(pt.numpy().decode('utf-8'))
+    print(en.numpy().decode('utf-8'))
 
-    # Print one sample from validation set
-    for pt, en in data.data_valid.take(1):
-        print(pt.numpy().decode('utf-8'))
-        print(en.numpy().decode('utf-8'))
+for pt, en in data.data_valid.take(1):
+    print(pt.numpy().decode('utf-8'))
+    print(en.numpy().decode('utf-8'))
 
-    # Print tokenizer types
-    print(type(data.tokenizer_pt))
-    print(type(data.tokenizer_en))
-
-
-if __name__ == "__main__":
-    main()
+print(type(data.tokenizer_pt))
+print(type(data.tokenizer_en))
