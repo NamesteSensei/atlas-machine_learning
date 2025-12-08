7260542cfc657c45dee941bae3b05eac60546894
namestesensei <christopherwright435@gmail.com>
2025-12-08 16:54:11 -0600

    3.1

--- supervised_learning/transformer_apps/3-dataset.py
+++ supervised_learning/transformer_apps/3-dataset.py
@@ -3,14 +3,21 @@
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from transformers import AutoTokenizer
+import transformers
 
 
 class Dataset:
-    """Loads data and tokenizers"""
+    """Loads translation dataset and builds pipeline"""
 
     def __init__(self, batch_size, max_len):
         """Initializes dataset, tokenizers, and pipeline"""
+        self.tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        self.tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased'
+        )
+
         self.data_train = tfds.load(
             'ted_hrlr_translate/pt_to_en',
             split='train',
@@ -22,25 +29,19 @@ class Dataset:
             as_supervised=True
         )
 
-        self.tokenizer_pt = AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased'
-        )
-        self.tokenizer_en = AutoTokenizer.from_pretrained(
-            'bert-base-uncased'
-        )
-
         self.data_train = self.data_train.map(self.tf_encode)
         self.data_valid = self.data_valid.map(self.tf_encode)
 
         self.data_train = self.data_train.filter(
             lambda pt, en: tf.logical_and(
-                tf.size(pt) <= max_len, tf.size(en) <= max_len
+                tf.size(pt) <= max_len,
+                tf.size(en) <= max_len
             )
         )
         self.data_train = self.data_train.cache()
         self.data_train = self.data_train.shuffle(20000)
         self.data_train = self.data_train.padded_batch(
-            batch_size, padding_values=(0, 0)
+            batch_size, padded_shapes=([None], [None])
         )
         self.data_train = self.data_train.prefetch(
             tf.data.experimental.AUTOTUNE
@@ -48,33 +49,35 @@ class Dataset:
 
         self.data_valid = self.data_valid.filter(
             lambda pt, en: tf.logical_and(
-                tf.size(pt) <= max_len, tf.size(en) <= max_len
+                tf.size(pt) <= max_len,
+                tf.size(en) <= max_len
             )
         )
         self.data_valid = self.data_valid.padded_batch(
-            batch_size, padding_values=(0, 0)
+            batch_size, padded_shapes=([None], [None])
         )
 
     def encode(self, pt, en):
-        """Encodes sentences with start and end tokens"""
-        pt_ids = self.tokenizer_pt.encode(
+        """Encodes Portuguese and English sentences"""
+        pt_tokens = self.tokenizer_pt.encode(
             pt.numpy().decode('utf-8'),
             add_special_tokens=True
         )
-        en_ids = self.tokenizer_en.encode(
+        en_tokens = self.tokenizer_en.encode(
             en.numpy().decode('utf-8'),
             add_special_tokens=True
         )
-        return tf.convert_to_tensor(pt_ids, dtype=tf.int64), \
-            tf.convert_to_tensor(en_ids, dtype=tf.int64)
+
+        return (
+            tf.convert_to_tensor(pt_tokens, dtype=tf.int64),
+            tf.convert_to_tensor(en_tokens, dtype=tf.int64)
+        )
 
     def tf_encode(self, pt, en):
-        """Wrapper for tf.py_function for encoding"""
-        result_pt, result_en = tf.py_function(
-            func=self.encode,
-            inp=[pt, en],
-            Tout=(tf.int64, tf.int64)
+        """TensorFlow wrapper for encode"""
+        pt_ids, en_ids = tf.py_function(
+            self.encode, [pt, en], [tf.int64, tf.int64]
         )
-        result_pt.set_shape([None])
-        result_en.set_shape([None])
-        return result_pt, result_en
+        pt_ids.set_shape([None])
+        en_ids.set_shape([None])
+        return pt_ids, en_ids
