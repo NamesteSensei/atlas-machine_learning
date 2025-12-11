bc3107429234c4b923b30237526ae5c5649b288a
namestesensei <christopherwright435@gmail.com>
2025-12-10 19:43:54 -0600

    changed 0-main.py

--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
+import tensorflow as tf
 
 Dataset = __import__('0-dataset').Dataset
-
 data = Dataset()
 
 for pt, en in data.data_train.take(1):
-    print(pt.numpy().decode('utf-8'))
-    print(en.numpy().decode('utf-8'))
+    print(type(pt) is tf.Tensor)
+    print(type(en) is tf.Tensor)
+    print(pt.dtype)
+    print(en.dtype)
 
-for pt, en in data.data_valid.take(2):
-    print(pt.numpy().decode('utf-8'))
-    print(en.numpy().decode('utf-8'))
+for pt, en in data.data_valid.take(1):
+    print(type(pt) is tf.Tensor)
+    print(type(en) is tf.Tensor)
+    print(pt.dtype)
+    print(en.dtype)
 
-print(type(data.tokenizer_pt))
-print(type(data.tokenizer_en))
