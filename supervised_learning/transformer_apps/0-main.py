8e1eef1d58aefb710dfc1f3d3105cda9aaa64ef4
namestesensei <christopherwright435@gmail.com>
2025-12-10 20:05:30 -0600

    changed files 2-main.py and 2-dataset.py

--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 
-Dataset = __import__('0-dataset').Dataset
+Dataset = __import__('2-dataset').Dataset
 data = Dataset()
 
 for pt, en in data.data_train.take(1):
-    print(type(pt) is tf.Tensor)
-    print(type(en) is tf.Tensor)
+    print(pt.dtype == tf.int64)
+    print(en.dtype == tf.int64)
     print(pt.dtype)
     print(en.dtype)
 
 for pt, en in data.data_valid.take(1):
-    print(type(pt) is tf.Tensor)
-    print(type(en) is tf.Tensor)
+    print(pt.dtype == tf.int64)
+    print(en.dtype == tf.int64)
     print(pt.dtype)
     print(en.dtype)
-
