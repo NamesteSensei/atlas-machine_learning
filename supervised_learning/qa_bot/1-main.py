1fd207a1d984a0895a027842f48c79b3fc9dbfe3
namestesensei <christopherwright435@gmail.com>
2025-12-24 23:13:24 -0600

    Add main script to test answer_loop from 1-qa.py

---
+++ supervised_learning/qa_bot/1-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+answer_loop = __import__('1-qa').answer_loop
+
+if __name__ == "__main__":
+    with open("ZendeskArticles/PeerLearningDays.md") as f:
+        reference = f.read()
+
+    answer_loop(reference)
