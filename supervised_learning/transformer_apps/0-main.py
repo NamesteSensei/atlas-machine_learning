24ad2ce2ef0fdf788b686adc4f6e85d72d1be350
namestesensei <christopherwright435@gmail.com>
2025-12-31 12:47:34 -0600

    edited task 0-main.py for 465 to 471 stdout expectation

--- supervised_learning/transformer_apps/0-main.py
+++ supervised_learning/transformer_apps/0-main.py
@@ -4,13 +4,17 @@ Dataset = __import__('0-dataset').Dataset
 
 data = Dataset()
 
+# Print one sample from training
 for pt, en in data.data_train.take(1):
     print(pt.numpy().decode('utf-8'))
     print(en.numpy().decode('utf-8'))
 
+# Print one sample from validation
 for pt, en in data.data_valid.take(1):
     print(pt.numpy().decode('utf-8'))
     print(en.numpy().decode('utf-8'))
 
+# Print tokenizer types
 print(type(data.tokenizer_pt))
 print(type(data.tokenizer_en))
+
