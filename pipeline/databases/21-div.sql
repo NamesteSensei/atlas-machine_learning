013a9a6aed1cce41c7989d616a57727bc6efcea0
namestesensei <christopherwright435@gmail.com>
2026-03-10 18:24:37 -0500

    Create SafeDiv function to safely divide integers

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,15 @@
+-- Create function SafeDiv that safely divides two integers
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+
+    RETURN a / b;
+END $$
+
+DELIMITER ;
