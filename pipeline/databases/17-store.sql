561004b9a039c0b760bd9365b015f27e7a946734
namestesensei <christopherwright435@gmail.com>
2026-03-10 17:34:46 -0500

    create trigger to decrease item quantiy after order insert

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,7 @@
+-- Create a trigger that  decreases item quantity after a new order is added
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
